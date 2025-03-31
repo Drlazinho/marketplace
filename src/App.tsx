5767eb10fadaece51bb10b1e5b998be828a6a048
Lázaro Pimentel <142930247+LazaroBonfim@users.noreply.github.com>
2025-03-31 01:50:52 -0300

    Autenticação e validação

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
+import { Toaster } from 'sonner'
 
 function App() {
   return (
     <>
       <HelmetProvider>
         <Helmet titleTemplate="%s | marketplace" />
-        <RouterProvider router={router} />
+        <Toaster richColors closeButton />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </>
   );
--- src/pages/auth/Sign-in.tsx
+++ src/pages/auth/Sign-in.tsx
@@ -2,21 +2,61 @@ import { Input } from "@/components/ui/input";
 import { Helmet } from "react-helmet-async";
 import mail from "@/assets/icon/mail-02.svg";
 import access from "@/assets/icon/access.svg";
-import { CustomButton } from '@/components/custom-button'
-import { MoveRight } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
+import { CustomButton } from "@/components/custom-button";
+import { MoveRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "@/api/sign-in";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
+
+const signInForm = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function goSignUp() {
-    navigate('/sign-up')
+    navigate("/sign-up");
   }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  });
+
+  const handleSignIn = async ({ email, password }: SignInForm) => {
+    try {
+      await authenticate({ email, password });
+
+      toast.success("Enviamos um link de autenticação para seu e-mail.", {
+        action: {
+          label: "Tentar Novamente",
+          onClick: () => handleSignIn({ email, password }),
+        },
+      });
+    } catch (error) {
+      toast.error("Credenciais Inválidas", {
+        description: error?.response?.data?.message,
+      });
+    }
+  };
 
   return (
     <>
       <Helmet title="Sign-in" />
       <form
+        onSubmit={handleSubmit(handleSignIn)}
         action=""
         className="bg-shape-white h-4/5 px-20 py-16 mt-10 m-6 rounded-3xl flex flex-col"
       >
@@ -26,31 +66,45 @@ export function SignIn() {
             Informe seu e-mail e senha para entrar
           </p>
         </div>
-        <div className='flex-grow'>
+        <div className="flex-grow">
+          <Input
+            label="E-mail"
+            icon={<img src={mail} width={24} />}
+            placeholder="Seu e-mail cadastrado"
+            type="email"
+            filled
+            className="mt-10 mb-4"
+            {...register("email")}
+          />
+          <Input
+            label="senha"
+            icon={<img src={access} width={24} />}
+            {...register("password")}
+            type="password"
+            placeholder="Sua senha de acesso"
+            filled
+          />
+          <CustomButton
+            disabled={isSubmitting}
+            type="submit"
+            endIcon={<MoveRight />}
+            className="bg-orange-base mt-10"
+          >
+            Acessar
+          </CustomButton>
+        </div>
 
-        <Input
-          label="E-mail"
-          icon={<img src={mail} width={24} />}
-          placeholder="Seu e-mail cadastrado"
-          type='email'
-          defaultValue="Text"
-          filled
-          className='mt-10 mb-4'
-        />
-        <Input
-          label="senha"
-          icon={<img src={access} width={24} />}
-          type="password"
-          placeholder="Sua senha de acesso"
-          defaultValue="Text"
-          filled
-        />
-          <CustomButton endIcon={<MoveRight />} className='bg-orange-base mt-10'>Acessar</CustomButton>
-          </div>
-
-        <div className='flex flex-col'>
-          <p className='mb-2'>Ainda não tem uma conta?</p>
-          <CustomButton endIcon={<MoveRight />} variant='outline' className='text-orange-base' onClick={goSignUp}>Cadastrar</CustomButton>
+        <div className="flex flex-col">
+          <p className="mb-2">Ainda não tem uma conta?</p>
+          <CustomButton
+            type="button"
+            endIcon={<MoveRight />}
+            variant="outline"
+            className="text-orange-base"
+            onClick={goSignUp}
+          >
+            Cadastrar
+          </CustomButton>
         </div>
       </form>
     </>
